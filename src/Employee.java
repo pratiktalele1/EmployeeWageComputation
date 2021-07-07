28d1fa28b147717ed071aa81cab92a9b5c0b0126
Pratik Talele <pratiktalele22@gmail.com>
2021-07-07 15:32:13 +0530

    DailyEmployeeWage function is added

--- src/Employee.java
+++ src/Employee.java
@@ -1,16 +1,29 @@
+import java.util.Scanner;
 
 public class Employee {
 	double randomNumber;
 	double isFullTime=1;
-
-	public Employee(double randomNumber) {
+	double wagePerHour;
+	double fullDayHour;
+	
+	public void DailyEmployeeWage() {
+		double dailyWage=wagePerHour*fullDayHour;
+		System.out.println("employee wage "+dailyWage +" RS");
+		
+	
+		
+	}
+	public Employee(double randomNumber,double wagePerHour,double fullDayHour) {
 		super();
 		this.randomNumber = randomNumber;
+		this.wagePerHour=wagePerHour;
+		this.fullDayHour=fullDayHour;
 	}
 	
 	public void attendance() {
 		if (isFullTime==randomNumber) {
         	System.out.println("Employee is present");
+        	DailyEmployeeWage();
         }
         else {
         	System.out.println("Employee is absent");
--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,10 +1,12 @@
 
 public class EmployeeWage {
 	public static void main(String[] args) {
-		
+		double wagePerHour=20;
+		double fullDayHour=8;
 		double randomNumber = Math.floor(Math.random()*10)%2; 
-		Employee checkAttendance=new Employee(randomNumber);
+		Employee checkAttendance=new Employee(randomNumber,wagePerHour,fullDayHour);
 		checkAttendance.attendance();
+		checkAttendance.DailyEmployeeWage();
         
 		
 	}
